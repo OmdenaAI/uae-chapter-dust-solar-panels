212e4501879c69dace08d77e1de1d3593a62a826
MyatKaung <42263948+MyatKaung@users.noreply.github.com>
2022-09-30 12:28:10 +0800

    Add files via upload

---
+++ src/tasks/task-3-deploy-visualize-report/Flask_Web_App/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+from keras.models import load_model
+
+import itertools
+from keras_preprocessing import image
+
+
+app = Flask(__name__)
+
+dic = {0 : 'CleanPV', 1 : 'DirtyPV'}
+
+model = load_model('solar_classify_finetune_final-2.h5')
+
+model.make_predict_function()
+
+def predict_label(img_path):
+    i = image.load_img(img_path, target_size=(224,224))
+    i = image.img_to_array(i)/255.0
+    i = i.reshape(1, 224,224,3)
+    p = model.predict(i)
+    merged = list(itertools.chain(*p))
+    key = ['CleanPV', 'DirtyPV']
+    res = dict(zip(key,merged))
+    final_res = list(res.keys())[0]
+    return final_res
+
+
+# routes
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	return render_template("index.html")
+
+@app.route("/about")
+def about_page():
+	return "Please subscribe  Artificial Intelligence Hub..!!!"
+
+@app.route("/submit", methods = ['GET', 'POST'])
+def get_output():
+	if request.method == 'POST':
+		img = request.files['my_image']
+
+		img_path = "static/" + img.filename	
+		img.save(img_path)
+
+		p = predict_label(img_path)
+
+	return render_template("index.html", prediction = p, img_path = img_path)
+
+
+if __name__ =='__main__':
+	#app.debug = True
+	app.run(debug = True)
